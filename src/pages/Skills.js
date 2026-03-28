f1604438c945f1e938b0b55e02699c95f9c6d757
Andy <colotilin.an@yandex.ru>
2026-03-28 17:23:26 +0300

    Изменение навыков в Backend

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -8,7 +8,7 @@ const Skills = () => {
 						<li className="content-lst__item"><h2 className="title-2">Frontend</h2>
 					<p>JavaScript, TypeScript, ReactJS, HTML, CSS, NPM, BootStrap, SCSS</p></li>
 						<li className="content-lst__item"><h2 className="title-2">Backend</h2>
-					<p>Node.js, MySQL, PHP</p></li>
+					<p>Node.js, PostgreSQL, Python</p></li>
 					</ul>
 			</div>
 		</main>
